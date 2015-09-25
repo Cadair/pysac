ef60018708752940b9848886e877d36ffed71722
Stuart Mumford <stuart@cadair.com>
2015-09-25 11:38:32 +0100

    thesis version - 0.2

--- pysac/plot/mayavi_plotting_functions.py
+++ pysac/plot/mayavi_plotting_functions.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This module provides a set of helper functions to make mayavi scenes into 
+This module provides a set of helper functions to make mayavi scenes into
 nice plots. It also provides routines for the visualisation of flux surfaces.
 """
 import numpy as np
@@ -10,10 +10,11 @@ from mayavi import mlab
 __all__ = ['set_text', 'set_cbar_text', 'add_axes', 'add_cbar_label',
            'add_colourbar', 'draw_surface', 'change_surface_scalars']
 
+
 def set_text(text_property):
     """
     Set the text to sane defaults
-    
+
     Parameters
     ----------
     text_property: mayavi TextProperty object
@@ -21,9 +22,10 @@ def set_text(text_property):
     text_property.bold = False
     text_property.italic = False
     text_property.font_family = 'times'
-    text_property.color = (0,0,0)
-    
-def set_cbar_text(cbar,lut_manager='scalar'):
+    text_property.color = (0, 0, 0)
+
+
+def set_cbar_text(cbar, lut_manager='scalar'):
     if lut_manager == 'scalar':
         manager = cbar.parent.scalar_lut_manager
     elif lut_manager == 'vector':
@@ -32,6 +34,7 @@ def set_cbar_text(cbar,lut_manager='scalar'):
         raise Exception()
     set_text(manager.title_text_property)
     set_text(manager.label_text_property)
+
 
 def add_axes(ranges, obj=None):
     axes1 = mlab.axes(obj)
@@ -50,20 +53,21 @@ def add_axes(ranges, obj=None):
     o.actor.actor.property.line_width = 1.5
     return axes1, o
 
+
 def add_cbar_label(cbar,title):
     position = cbar.scalar_bar_representation.position
     position2 = cbar.scalar_bar_representation.position2
-    
+
     x = position[0] + position2[0] #+ 0.002
     y = position[1]
-    
+
     text = mlab.text(x,y,title)
     text.property.font_size = 24
     set_text(text.property)
-    
+
     text.property.orientation = 90.
     text.width = 0.205
-    
+
     return text
 
 
@@ -104,56 +108,56 @@ def draw_surface(surf_poly, cmap, scalar='vperp', lines=False, lim=None, log10=F
                  **colourbar_args):
     """
     Draw a mayavi surface from a PolyData object.
-    
+
     Parameters
     ----------
     surf_poly: tvtk.PolyData Object
         The polydata object containing the surface information.
-    
+
     cmap: ndarray or string
         The colour map to scale the scalar data with.
-    
+
     scalar: string
         The name of the tvtk scalar to plot.
-    
+
     lines: (optional) bool
         If True then lines are not removed from the PolyData object and will be plotted.
-    
+
     lim: [None, float, int or [min,max]]
         If lim is None symmetric limits will be created covering the whole dynamic range,
         if the input is a number symmetric limts will e made with that as a maximum,
         if a [min,max] pair is passed that will be the limits.
-    
+
     log10: bool
         Scale the scalar with log scaling.
-    
+
     colorbar_label: string
         Label the colorbar.
-    
+
     **colorar_args: dict
         Other kwargs will be passed to mayavi_plotting_functions.add_colourbar()
-    
+
     Returns
     -------
     new_tube: mayavi.modules.surface.Surface
         The surface object
-    
+
     surf_bar: ScalarBarWidget
         The colorbar object
-    
+
     surf_bar_label: mayavi.modules.text.Text
         The object created to label the colorbar
     """
     #Remove lines
     if not lines:
         surf_poly.lines = None
-    
+
     #Create the surface
     new_tube = mlab.pipeline.surface(surf_poly)
-    
+
     #Set the surface scalar
     new_tube.parent.parent.point_scalars_name = scalar
-    
+
     #Parse the colour map
     if isinstance(cmap, np.ndarray):
         new_tube.module_manager.scalar_lut_manager.lut.table = cmap
@@ -183,7 +187,7 @@ def draw_surface(surf_poly, cmap, scalar='vperp', lines=False, lim=None, log10=F
     lim = _parse_limits(lim)
     new_tube.module_manager.scalar_lut_manager.data_range = np.array(lim)
 
-    
+
     return new_tube, surf_bar, surf_bar_label
 
 def change_surface_scalars(new_tube, surf_bar_label, scalar_name, colorbar_label=None,
@@ -191,29 +195,29 @@ def change_surface_scalars(new_tube, surf_bar_label, scalar_name, colorbar_label
     """
     Change the surface scalars of an existing surface object, and change the associated
     colorbar and text objects. Note: This is hard coded to be easier for velocity surfaces.
-    
+
     Parameters
     ----------
     new_tube: mayavi.modules.surface.Surface
         The Surface
-    
+
     surf_bar_label: mayavi.modules.text.Text
         The text object
 
     scalar_name: string
         The name to set the scalar to.
-    
+
     colorbar_label: string
         The string to set the colorbar tex object to, if None it will guess velocity ones.
-    
+
     lim:[None, float, int or [min,max]]
         If lim is None symmetric limits will be created covering the whole dynamic range,
         if the input is a number symmetric limts will e made with that as a maximum,
         if a [min,max] pair is passed that will be the limits.
-    
+
     log10: bool
         Scale the scalar with log scaling.
-    
+
     Returns
     -------
     None
@@ -230,7 +234,7 @@ def change_surface_scalars(new_tube, surf_bar_label, scalar_name, colorbar_label
     new_tube.parent.scalar_lut_manager.data_name = ''
     surf_bar_label.text = title
     surf_poly = new_tube.parent.parent.outputs[0]
-    
+
     #Parse lim
     if lim is None:
         lim = np.max([np.nanmax(surf_poly.point_data.scalars),
@@ -238,9 +242,9 @@ def change_surface_scalars(new_tube, surf_bar_label, scalar_name, colorbar_label
         lim = [-lim,lim]
     lim = _parse_limits(lim)
     new_tube.module_manager.scalar_lut_manager.data_range = np.array(lim)
-    
+
     if log10:
         new_tube.module_manager.scalar_lut_manager.lut.scale = 'log10'
     if not log10:
         new_tube.module_manager.scalar_lut_manager.lut.scale = 'linear'
-    
+
